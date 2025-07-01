e2e6e42eafc1ac643e2a9660d27213149989725a
Kobi Begas <kobib@yaelsoft.com>
2025-07-01 13:06:16 +0300

    add python-app with k8s deployments

---
+++ src/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+import datetime, socket
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/details')
+
+def details():
+    return jsonify({
+        "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "hostname":  socket.gethostname()
+    })
+
+@app.route('/api/v1/health')
+
+def health():
+    return jsonify({
+        "status": "up"
+    }), 200
+
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0")
