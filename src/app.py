7a8e1b48c191c025493d71ea83d90504cc16a3c9
Kobi Begas <kobib@yaelsoft.com>
2025-07-03 14:08:16 +0300

    add cd job

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify({
         "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "hostname":  socket.gethostname(),
-	"message": "OK!!! + self-hosted"
+	"message": "OK!!! - cicd"
     })
 
 @app.route('/api/v1/health')
