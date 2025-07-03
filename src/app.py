164e069a4d81d5845c7061dc5aeeb862aa2059fe
Kobi Begas <kobib@yaelsoft.com>
2025-07-03 15:37:05 +0300

    modify values file on cd step

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify({
         "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "hostname":  socket.gethostname(),
-        "message": "OK: cicd, argo installed"
+        "message": "CICD Done!"
     })
 
 @app.route('/api/v1/health')
